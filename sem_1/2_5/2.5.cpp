a518c0c17b1121ec39defc75a2b4a46b227c1713
sulatevamarina374-del <sulatevamarina374@gmail.com>
2025-12-07 14:53:55 +0500

    Add files via upload

---
+++ sem_1/2_5/2.5.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cin >> n;
+	if (n < 2)
+	{
+		cout << "fail" << endl;
+	}
+	else
+	{
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 1; j < n - 1; j++)
+			{
+				cout << " ";
+			}
+			for (int j = 0; j <= i; j++)
+			{
+				cout << "*";
+			}
+			cout << endl;
+		}
+	}
+}
