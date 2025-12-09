1195769a36a92f9b4a3932bdc8f5d3c6081264db
sulatevamarina374-del <sulatevamarina374@gmail.com>
2025-12-09 16:18:35 +0500

    Add files via upload

---
+++ sem_1/2_8/2.8.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	if (n < 2)
+	{
+		cout << "fail" << endl;
+	}
+	else
+	{
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j <= i; j++)
+			{
+				cout << "* ";
+			}
+			cout << endl;
+		}
+	}
+	return 0;
+}
