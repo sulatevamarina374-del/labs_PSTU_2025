b560a0bcd56f524476672b5e5c0dcf7747aea835
sulatevamarina374-del <sulatevamarina374@gmail.com>
2025-12-16 16:17:55 +0500

    Add files via upload

---
+++ sem_1/2__18/2..18.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	int n;
+	int r = 0;
+
+	cin >> n;
+
+	while (n > 0)
+	{
+		r = r * 10 + n % 10;
+		n /= 10;
+	}
+	cout << r << endl;
+
+	return 0;
+}
