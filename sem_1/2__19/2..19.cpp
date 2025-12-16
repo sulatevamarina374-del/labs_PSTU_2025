f324837d6b108aa8623b9f8b1c6438fc328f763c
sulatevamarina374-del <sulatevamarina374@gmail.com>
2025-12-16 16:18:29 +0500

    Add files via upload

---
+++ sem_1/2__19/2..19.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	int n;
+	int sum = 0;
+
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+	{
+		if (i % 3 == 0)
+		{
+			sum -= i;
+		}
+		else
+		{
+			sum += i;
+		}
+	}
+	cout << sum << endl;
+
+	return 0;
+}
