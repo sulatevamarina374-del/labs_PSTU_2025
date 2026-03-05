ce0aec74d42ff2f8352c1acb854820f66237b429
sulatevamarina374-del <sulatevamarina374@gmail.com>
2026-03-05 20:17:52 +0500

    Add files via upload

---
+++ sem_2/3/3_12/3__12.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	const int n = 3;
+	int arr[n][n] = {
+	{1,2,3},
+	{4,5,6},
+	{7,8,9}
+	};
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+		cout << arr[i][j] << " ";
+		}
+		cout << endl;
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		arr[i][n - 1 - i] = 0;
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+	for (int j = 0; j < n; j++)
+		{
+		cout << arr[i][j] << " ";
+		}
+		cout << endl; 
+	}
+
+	return 0;
+}
